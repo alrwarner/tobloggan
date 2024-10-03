13eed85ebc24664e172b5415a680a78e2ca05270
CarolineHickey <caroline@smarty.com>
2024-10-03 11:55:46 -0600

    stations 2 & 3

--- code/stations/02_article_parser.go
+++ code/stations/02_article_parser.go
@@ -1,11 +1,45 @@
 package stations
 
-//type ArticleParser struct{}
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
 
-//func (this *ArticleParser) Do(input any, output func(any)) {
-//    TODO: given a contracts.SourceFile, parse the JSON metadata and save the body on a contracts.Article.
-//    input: contracts.SourceFile
-//    input: contracts.Article
-//}
+	"tobloggan/code/contracts"
+)
 
-//var errMalformedContent = errors.New("malformed content")
+type ArticleParser struct{}
+
+// TODO: given a contracts.SourceFile, parse the JSON metadata and save the body on a contracts.Article.
+// input: contracts.SourceFile
+// output: contracts.Article
+
+func (this *ArticleParser) Do(input any, output func(any)) {
+	switch input := input.(type) {
+	case contracts.SourceFile:
+		var article contracts.Article
+		myString := string(input)
+
+		// convert the bytes to string
+		parts := strings.Split(myString, "+++")
+
+		// json unmarshall to an contracts.Article
+		err := json.Unmarshal([]byte(parts[0]), &article)
+		if err != nil {
+			error := fmt.Errorf("Error unmarchsaling JSON: %w : %w", err, errMalformedContent)
+			output(error)
+			return // todo: needed?
+		}
+
+		// Assign body of input to body of contracts.Article
+		article.Body = parts[1]
+
+		// Output the created contracts.Article
+		output(article)
+	default:
+		output(input)
+	}
+}
+
+var errMalformedContent = errors.New("malformed content")
--- code/stations/03_article_validator.go
+++ code/stations/03_article_validator.go
@@ -1,16 +1,55 @@
 package stations
 
 import (
-	"tobloggan/code/set"
+	"tobloggan/code/contracts"
 )
 
-//type ArticleValidator struct {
-//}
+type ArticleValidator struct{}
 
-//func (this *ArticleValidator) Do(input any, output func(any)) {
-//    TODO: given a contracts.Article, validate the Slug and the Title fields and emit the contracts.Article (or an error)
-//    input: contracts.Article
-//    output: contracts.Article (or error)
-//}
+func (this *ArticleValidator) Do(input any, output func(any)) {
+	//    TODO: given a contracts.Article, validate the Slug and the Title fields and emit the contracts.Article (or an error)
+	//    input: contracts.Article
+	//    output: contracts.Article (or error)
 
-var validSlugCharacters = set.New([]rune("abcdefghijklmnopqrstuvwxyz0123456789-/")...)
+	switch input := input.(type) {
+	case contracts.Article:
+
+		// Check the Title of the contracts.Article
+		if len(input.Title) == 0 {
+			output(errMalformedContent)
+		} else if len(input.Title) > 150 {
+			output(errMalformedContent)
+		}
+
+		// check slug characters are valid
+		if !isValidSlug(input.Slug, validSlugCharacters) {
+			output(errMalformedContent)
+		}
+
+		// TODO: Check date range
+
+		// Output article, there were no problems
+		output(input)
+	default:
+		output(input)
+	}
+}
+
+func newRuneSet(chars string) map[rune]struct{} {
+	set := make(map[rune]struct{})
+	for _, character := range chars { // loop over the characters provided that are valid
+		set[character] = struct{}{} // store each valid character into a map
+	}
+	return set
+}
+
+func isValidSlug(slug string, validSlugCharacters map[rune]struct{}) bool {
+	for _, character := range slug { // range over all the characters in the slug
+		if _, exists := validSlugCharacters[character]; !exists {
+			return false // Character not found in valid characters
+		}
+	}
+	return true
+}
+
+var validSlugCharacters = newRuneSet("abcdefghijklmnopqrstuvwxyz0123456789-/")

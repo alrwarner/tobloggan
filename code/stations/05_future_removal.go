e598b809ee1e50d597cacf7658bee5d07258e65b
Adam Charlton <adam@smarty.com>
2024-10-03 11:17:20 -0600

    create content filters for draft and future blog posts

--- code/stations/05_future_removal.go
+++ code/stations/05_future_removal.go
@@ -1,7 +1,23 @@
 package stations
 
-//type FutureRemoval struct{}
+import (
+	"time"
 
-//func (this *FutureRemoval) Do(input any, output func(any)) {
-//    TODO: given a contracts.Article, only output it if the Date field is not after the current time (passed into constructor).
-//}
+	"tobloggan/code/contracts"
+)
+
+type FutureRemoval struct{}
+
+func (this *FutureRemoval) Do(input any, output func(any)) {
+	//    TODO: given a contracts.Article, only output it if the Date field is not after the current time (passed into constructor).
+
+	switch input := input.(type) {
+	case contracts.Article:
+		if !input.Date.After(time.Now()) {
+			output(input)
+		}
+	default:
+		output(input)
+	}
+
+}

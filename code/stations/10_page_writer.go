1be89410756fee24112bab2f201daf28bf51f779
Adam Charlton <adam@smarty.com>
2024-10-03 11:53:30 -0600

    create step 10 for content directory

--- code/stations/10_page_writer.go
+++ code/stations/10_page_writer.go
@@ -1,8 +1,33 @@
 package stations
 
-//type PageWriter struct {}
-//
-//func (this *PageWriter) Do(input any, output func(any)) {
-//    TODO: given a contracts.Page, create a directory at contracts.Page.Path then
-//    write contracts.Page.Content to filepath.Join(this.targetDirectory, input.Path, "index.html")
-//}
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+
+	"tobloggan/code/contracts"
+)
+
+type PageWriter struct {
+	targetDirectory string
+}
+
+func (this *PageWriter) Do(input any, output func(any)) {
+	//    TODO: given a contracts.Page, create a directory at contracts.Page.Path then
+	//    write contracts.Page.Content to filepath.Join(this.targetDirectory, input.Path, "index.html")
+
+	switch input := input.(type) {
+	case contracts.Page:
+		fPath := filepath.Join(this.targetDirectory, input.Path, "index.html")
+		os.MkdirAll(fPath, os.ModePerm)
+		outputFile, _ := os.Create(fPath)
+		writer := bufio.NewWriter(outputFile)
+		writer.WriteString(input.Content)
+		writer.Flush()
+		outputFile.Close()
+		output(input)
+	default:
+		output(input)
+	}
+
+}

ec252f4e9eeab572469196097dc5097dabc669f1
Adam Charlton <adam@smarty.com>
2024-10-03 11:29:47 -0600

    create the article listing sorting

--- code/stations/07_listing_renderer.go
+++ code/stations/07_listing_renderer.go
@@ -1,12 +1,33 @@
 package stations
 
-//type ListingRenderer struct {}
-//
-//func (this *ListingRenderer) Do(input any, output func(any)) {
-//    TODO: given a contracts.Article, append it to a slice and send it on
-//}
-//func (this *ListingRenderer) Finalize(output func(any)) {
-//    TODO: sort the slice (by Date), generate a <li> for each article in a big string,
-//    TODO: pageContent := strings.Replace(this.template, "{{Listing}}", renderedListing, 1)
-//    TODO: output(contracts.Page{Path: "/", Content: pageContent})
-//}
+import (
+	"sort"
+
+	"tobloggan/code/contracts"
+)
+
+type ListingRenderer struct {
+	articleListing []contracts.Article
+}
+
+func (this *ListingRenderer) Do(input any, output func(any)) {
+	//    TODO: given a contracts.Article, append it to a slice and send it on
+	switch input := input.(type) {
+	case contracts.Article:
+		this.articleListing = append(this.articleListing, input)
+		output(input)
+	default:
+		output(input)
+	}
+
+}
+func (this *ListingRenderer) Finalize(output func(any)) {
+	//    TODO: sort the slice (by Date), generate a <li> for each article in a big string,
+	//    TODO: pageContent := strings.Replace(this.template, "{{Listing}}", renderedListing, 1)
+	//    TODO: output(contracts.Page{Path: "/", Content: pageContent})
+
+	sort.Slice(this.articleListing, func(i, j int) bool {
+		return this.articleListing[i].Date.Before(this.articleListing[j].Date)
+	})
+	output(this.articleListing)
+}
